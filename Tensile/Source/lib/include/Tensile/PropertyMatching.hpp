170d3d58ab5ee87879c94cc12a23779a262ea568
Guillermo Trabes <gutrabes@amd.com>
2024-05-05 21:01:36 -0400

    Fixed error for kernel returned

--- Tensile/Source/lib/include/Tensile/PropertyMatching.hpp
+++ Tensile/Source/lib/include/Tensile/PropertyMatching.hpp
@@ -248,7 +248,6 @@ namespace Tensile
                         }
                         else if (currentGranularityLoss == bestGranularityLoss){
                             bestKernelVector.push_back(currentKernel);
-                            bestGranularityLoss = currentGranularityLoss;
                         }
                         
                         
@@ -256,7 +255,7 @@ namespace Tensile
                 }
                 //get kernel that worked with GEMM with lowest euclidean distance
                 //bestKernel = findLowerEuclidenDistance(bestKernelVector);
-                bestKernel = bestKernelVector.back();
+                bestKernel = bestKernelVector[0];
                 return bestKernel;
             }
 
